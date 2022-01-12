e93eca3d5ea266e7db9940e704f81b9fe15e43ae
Déborah <deborahtrota@gmail.com>
2022-01-12 11:39:37 -0300

    chooses the background color with a picker

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-const randomButton = document.getElementById('randomButton');
-const pickButton = document.getElementById('pickButton');
+//Generates a random background color when clicked
 
 function randomColor() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
-    var color = "rgb(" + x + "," + y + "," + z + ")"
+    var color = "rgb(" + x + "," + y + "," + z + ")";
 
     document.body.style.backgroundColor = color;
+}
+
+//User can pick the background color in a picker 
+
+function pickColor() {
+    let colorPicker = document.getElementById('colorPicker');
+    colorPicker.addEventListener("change", function () {
+        document.body.style.backgroundColor = this.value;
+    });
 }
